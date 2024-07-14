1deb4a8a9cf59e81b0567d6fe11e4e0e2b43d87c
sophiaAndMe <jjlaverde@uce.edu.ec>
2024-07-14 14:19:50 -0500

    añadi el login

---
+++ src/main/java/org/uce/app/controllers/loginController.java
@@ -0,0 +1,30 @@
+package org.uce.app.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class loginController {
+
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    void eventAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eventKey(KeyEvent event) {
+
+    }
+}
