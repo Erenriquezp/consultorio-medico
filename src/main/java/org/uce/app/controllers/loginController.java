bfc31d1b6ebf346dcbbb36f6c59c20ae3e04a004
sophiaAndMe <jjlaverde@uce.edu.ec>
2024-07-16 14:39:01 -0500

    .fxml, .java de login

--- src/main/java/org/uce/app/controllers/loginController.java
+++ src/main/java/org/uce/app/controllers/loginController.java
@@ -14,43 +14,12 @@ import java.io.IOException;
 
 public class loginController {
 
-    // botton de login
     @FXML
-    private Button btnLogin;
-
-    //
-    @FXML
-    private TextField txtPassword;
+    private TextField passwordField;
 
     @FXML
-    private TextField txtUser;
+    private TextField userField;
 
-    //-- Metodo cuando aplastas el botton 'ingresar'
-    @FXML
-    private void eventAction(ActionEvent event) {
-    //cargarPantalla(Paths.pantallaPrincipal, "Pantalla Principal");
-    }
-    //-- Metodo para cuando se ingresa el User y Password
-    @FXML
-    void eventKey(KeyEvent event) {
 
-    }
-    // Método privado para cargar una nueva pantalla
-//    private void cargarPantalla(String fxmlPath, String titulo) {
-//        try {
-//            // Crea un FXMLLoader con la ruta del archivo FXML
-//            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
-//            // Crea un nuevo Stage (ventana)
-//            Stage stage = new Stage();
-//            // Establece el título de la ventana
-//            stage.setTitle(titulo);
-//            // Carga el archivo FXML y establece la escena en el Stage
-//            stage.setScene(new Scene(loader.load()));
-//            // Muestra la ventana
-//            stage.show();
-//        } catch (IOException e) {
-//            // Manejo de la excepción en caso de error al cargar el archivo FXML
-//            e.printStackTrace();
-//        }
-//    }
+
 }
