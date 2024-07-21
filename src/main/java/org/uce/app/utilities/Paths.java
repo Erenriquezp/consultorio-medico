3139de5c7b378e53ff5f2e025989f8cd95e759f4
sophiaAndMe <jjlaverde@uce.edu.ec>
2024-07-20 23:30:01 -0500

    Ya le cambie el nombre HistoriaClinica.fxml

--- src/main/java/org/uce/app/utilities/Paths.java
+++ src/main/java/org/uce/app/utilities/Paths.java
@@ -4,7 +4,7 @@ public class Paths {
     public static final String gestionCitas = "/fxml/GestionCitas.fxml";
     public static final String pantallaPrincipal = "/fxml/PantallaPrincipal.fxml";
     public static final String gestionPacientes = "/fxml/GestionPacientes.fxml";
-    public static final String historialMedico = "/fxml/HistorialMedico.fxml";
+    public static final String historialMedico = "/fxml/HistoriaClinica.fxml";
     public static final String loginScreen = "/fxml/Login.fxml";
     public static final String stylesheet = "/styles/styles.css";
 }
